af437f243da22ba825d872760ad02e1d65b66a48
Sergey Shappo <shappoff@gmail.com>
2024-11-30 00:24:20 +0300

    up routing

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
     return (
         <div>
             <h1>My Family Tree</h1>
-            <a href={`${pathname}me`}>I&apos;m</a>
+            <a href={`${pathname}/me`}>I&apos;m</a>
         </div>
     )
 }
