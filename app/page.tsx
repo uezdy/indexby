560ee85228b9bbbb24f5e0f370e4c9608fb30d4f
Sergey Shappo <shappoff@gmail.com>
2024-11-29 11:09:50 +0300

    init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Button} from "@/components/button";
+import '@/styles/styles.css'
+
+export default function Page() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <Button />
+        </div>
+    )
+}
