5404f6e8a4197f064689a6177168bcd639a1060e
Sergey Shappo <shappoff@gmail.com>
2024-11-30 00:20:51 +0300

    up routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,13 @@
 'use client'
 
-import React from 'react';
-import type { Metadata } from 'next'
 import { usePathname } from 'next/navigation';
-import {Button} from "@/components/button";
-import '@/styles/styles.css'
-
-export const metadata: Metadata = {
-    title: 'My family tree',
-    description: 'My family tree'
-}
 
 export default function Page() {
     const pathname = usePathname();
     return (
         <div>
             <h1>My Family Tree</h1>
-            <a href={`${pathname}/me`}>I&apos;m</a>
+            <a href={`${pathname}me`}>I&apos;m</a>
         </div>
     )
 }
