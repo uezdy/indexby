c78d3f85ba70c0da75b3bc43332f58d5669f2283
Sergey Shappo <shappoff@gmail.com>
2024-12-13 22:33:31 +0300

    add names

--- app/metrics/map/page.tsx
+++ app/metrics/map/page.tsx
@@ -1,9 +1,15 @@
 'use client'
 import dynamic from 'next/dynamic';
-const LeafletInfoMap = dynamic(() => import("@/components/LeafletInfoMap"), { ssr: false })
+// const LeafletInfoMap = dynamic(() => import("@/components/LeafletInfoMap"), { ssr: false });
+import React from "react";
 
 export default function MapMetrics() {
-    return <>
-        <LeafletInfoMap />
-    </>
+    const LeafletInfoMap = React.useMemo(() => dynamic(
+        () => import("@/components/LeafletInfoMap"),
+        {
+            loading: () => <p>A map is loading</p>,
+            ssr: false
+        }
+    ), [])
+    return <LeafletInfoMap />
 }
