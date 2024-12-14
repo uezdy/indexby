7d73e1e849ef197b4819dde9431dac7602e8ed31
Sergey Shappo <shappoff@gmail.com>
2024-12-14 22:23:23 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+import Link from 'next/link';
+
+import List from '@mui/joy/List';
+import ListItem from '@mui/joy/ListItem';
+import ListItemButton from '@mui/joy/ListItemButton';
+import Home from '@mui/icons-material/Home';
+
 export default function Page() {
-    return (
-        <div>
-            <h1>My Family Tree</h1>
-            <a href="/me">I&apos;m</a>
-        </div>
-    )
+    return <>
+        <List
+            size="lg"
+            variant="outlined"
+            sx={{ maxWidth: 300, borderRadius: 'sm' }}
+        >
+            <ListItem><ListItemButton><Link href="/"><Home /></Link></ListItemButton></ListItem>
+            <ListItem><ListItemButton><Link href="/names">Имена</Link></ListItemButton></ListItem>
+            <ListItem><ListItemButton><Link href="/metrics">Метрические книги</Link></ListItemButton></ListItem>
+        </List>
+    </>
 }
