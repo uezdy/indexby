7dc34ad4291382db1e89b118f741f1c04b60d109
Sergey Shappo <shappoff@gmail.com>
2024-12-24 22:28:07 +0300

    update icons

---
+++ app/api/route.tsx
@@ -0,0 +1,3 @@
+export async function GET(req, res) {
+    res.status(200).json({ text: 'Hello' });
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
             <meta charSet="UTF-8"></meta>
             <meta name="robots" content="index, follow"></meta>
             <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+            
             <title></title>
         </head>
         <body style={{margin: '0'}}>
